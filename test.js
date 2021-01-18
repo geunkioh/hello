9b8545681dcce620120d68c8739ff17713ef2872
geunkioh <kunkiya@gmail.com>
2021-01-18 18:08:03 +0900

    Add files via upload

--- test.js
+++ test.js
@@ -1,29 +1,36 @@
-//lotto.js
+let tmp = 1;
+let numbers;
 
-for (let a = 1; a < 6; a++) {           //로또번호가 총 5번 나오도록 하는 for문
+function decryptEffect(element, time) {
+    const effect = setInterval(() => {
+        element.innerText = Math.floor(Math.random() * 44 + 1);
+    }, 10);
 
-    let Num = [];                           //중복되지 않은 1에서 45의 숫자들이 6개 들어감
+    setTimeout(() => {
+        const random = Math.floor(Math.random() * numbers.length);
 
-    for (let j = 0; j < 999; j++)           //중복된 숫자가 나올 수 있으므로 넉넉하게 999번 반복
-    {
-        let randomNum = Math.floor(Math.random() * 45) + 1;  //1부터 45의 랜덤한 숫자가 생성됨
-        {
-            if (Num.indexOf(randomNum) == -1)      //num에 들어간 숫자중에 randomNum의 숫자와 다른지 확인
-
-                Num.push(randomNum);                //다르다면 num배열에 randomNum을 넣는다
-            if (Num.length == 6) {
-                break;                            //num의 길이가 6이 되면 멈춤
-            }
-        }
-    }
-
-
-    function sortNum(a, b)  //로또 숫자들을 오름차순으로 정렬 해주는 함수
-    {
-        return a - b;
-    }
-
-    Num.sort(sortNum);     //로또 번호들이 오름차순으로 나온다
-
-    console.log(a + "번째로또 번호는" + "[" + Num + "]");
+        clearInterval(effect);
+        element.classList.add("done");
+        element.innerText = numbers[random];
+        numbers.splice(random, 1);
+    }, time * 1000 + 1000);
 }
+
+function lottery() {
+    document.querySelectorAll(".ball").forEach((element, index) => {
+        element.classList.remove("done");
+        decryptEffect(element, index);
+    });
+}
+
+document.getElementById("btn").addEventListener("click", function () {
+    if (tmp === 1) {
+        numbers = Array.from({ length: 45 }, (_, i) => i + 1);
+        tmp = 0;
+        btn.classList.add("hide");
+        lottery();
+        setTimeout(function () {
+            (tmp = 1), btn.classList.remove("hide");
+        }, 7500);
+    }
+});
